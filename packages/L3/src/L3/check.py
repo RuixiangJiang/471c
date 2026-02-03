de4803d2da3e385c41da6e79be4c8c4342b26f3d
James Clause <clause@udel.edu>
2026-02-03 13:09:03 -0500

    Add check/testing scaffold for L3

---
+++ packages/L3/src/L3/check.py
@@ -0,0 +1,65 @@
+from collections.abc import Mapping
+from functools import partial
+
+from .syntax import (
+    Abstract,
+    Allocate,
+    Apply,
+    Begin,
+    Branch,
+    Identifier,
+    Immediate,
+    Let,
+    LetRec,
+    Load,
+    Primitive,
+    Reference,
+    Store,
+    Term,
+)
+
+type Context = Mapping[Identifier, None]
+
+
+def check_term(
+    term: Term,
+    context: Context,
+) -> None:
+    recur = partial(check_term, context=context)  # noqa: F841
+
+    match term:
+        case Let():
+            pass
+
+        case LetRec():
+            pass
+
+        case Reference():
+            pass
+
+        case Abstract():
+            pass
+
+        case Apply():
+            pass
+
+        case Immediate():
+            pass
+
+        case Primitive():
+            pass
+
+        case Branch():
+            pass
+
+        case Allocate():
+            pass
+
+        case Load():
+            pass
+
+        case Store():
+            pass
+
+        case Begin():  # pragma: no cover
+            pass
---
+++ packages/L3/test/L3/test_check.py
@@ -0,0 +1,22 @@
+import pytest
+from L3.check import Context, check_term
+from L3.syntax import Reference
+
+
+def test_check_reference_bound():
+    term = Reference(name="x")
+
+    context: Context = {
+        "x": None,
+    }
+
+    check_term(term, context)
+
+
+def test_check_reference_free():
+    term = Reference(name="x")
+
+    context: Context = {}
+
+    with pytest.raises(ValueError):
+        check_term(term, context)
